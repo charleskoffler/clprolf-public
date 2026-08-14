f0930b81c4d358209dc9fea531206908ac66e688
Sabri <sabrikoffler@wanadoo.fr>
2026-08-14 13:16:16 +0200

    Updated .NET graphs example

--- examples/dotnet/Clprolf.Examples.Graphs/Clprolf.ArchUnitNet/Rules/ClprolfTypeExtension.cs
+++ examples/dotnet/Clprolf.Examples.Graphs/Clprolf.ArchUnitNet/Rules/ClprolfTypeExtension.cs
@@ -14,16 +14,29 @@ internal static class ClprolfTypeExtensions
             attribute.Name == attributeType.Name);
     }
 
-    public static bool IsAgent(this IType type) => type.HasClAttribute(typeof(ClAgentAttribute));
-    public static bool IsWorker(this IType type) => type.HasClAttribute(typeof(ClWorkerAttribute));
-    public static bool IsSystem(this IType type) => type.HasClAttribute(typeof(ClSystemAttribute));
+    // Role detection including native aliases
+    public static bool IsAgent(this IType type) =>
+        type.HasClAttribute(typeof(ClAgentAttribute)) ||
+        type.HasClAttribute(typeof(ClConceptAttribute)) ||
+        type.HasClAttribute(typeof(ClDomainAttribute));
+
+    public static bool IsWorker(this IType type) =>
+        type.HasClAttribute(typeof(ClWorkerAttribute)) ||
+        type.HasClAttribute(typeof(ClMechanismAttribute)) ||
+        type.HasClAttribute(typeof(ClInfrastructureAttribute));
+
+    public static bool IsSystem(this IType type) =>
+        type.HasClAttribute(typeof(ClSystemAttribute)) ||
+        type.HasClAttribute(typeof(ClBridgeAttribute)) ||
+        type.HasClAttribute(typeof(ClLowLevelAttribute));
+
     public static bool IsDraft(this IType type) => type.HasClAttribute(typeof(ClDraftAttribute));
     public static bool IsFamily(this IType type) => type.HasClAttribute(typeof(ClFamilyAttribute));
     public static bool IsTrait(this IType type) => type.HasClAttribute(typeof(ClTraitAttribute));
     public static bool IsFree(this IType type) => type.HasClAttribute(typeof(ClFreeAttribute));
 
     /// <summary>
-    /// Checks if the type belongs to the Clprolf semantic model by having at least one core structural attribute.
+    /// Checks if the type belongs to the Clprolf semantic model by having at least one core structural attribute or alias.
     /// </summary>
     public static bool IsClprolf(this IType type)
     {
